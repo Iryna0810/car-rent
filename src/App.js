ef35c24f947c522b87489ba889f049652de2e567
Rew <irinavakulenko2000@gmail.com>
2023-09-14 23:44:08 +0300

    routes create

--- src/App.js
+++ src/App.js
@@ -1,10 +1,41 @@
-// import logo from './logo.svg';
-// import './App.css';
+import React from 'react';
+import styled from "styled-components";
+import { Routes, Route, NavLink } from "react-router-dom";
+import Cars from './pages/Cars';
+import Home from './pages/Home';
+import CarsFavourite from './pages/CarsFavourite';
+
+const StyledLink = styled(NavLink)`
+  color: black;
+
+  &.active {
+    color: orange;
+  }
+`;
 
 function App() {
   return (
     <div>
-      <p>Hello world!</p>
+      <nav>
+        <ul>
+           <li>
+        <StyledLink to='/'>Home</StyledLink>
+          </li>
+          <li>
+        <StyledLink to='/Cars'>Cars</StyledLink>
+          </li>
+          <li>
+        <StyledLink to='/CarsFavourite'>CarsFavourite</StyledLink>
+          </li>
+        </ul>
+
+      </nav>
+      <Routes>
+        <Route path="/" element={<Home name='This is Home page' />}/>
+              <Route path="/Cars" element={<Cars name='This is Cars page'/>} />
+        <Route path="/CarsFavourite" element={ <CarsFavourite name='This is CarsFavourite page'/>} />
+      </Routes>
+
       </div>
   );
 }
---
+++ src/pages/Home.jsx
@@ -0,0 +1,12 @@
+import React from "react"
+
+const Home = ({name}) => {
+    return (
+    <div>
+            <h2>{name }</h2>
+    </div>
+    )
+
+}
+
+export default Home
