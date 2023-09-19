e7bf53398ae7b5ea9459bfc29bcb544cb0e92cc0
Rew <irinavakulenko2000@gmail.com>
2023-09-19 23:42:25 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,4 @@
 import React from 'react';
-// import styled from "styled-components";
-import { useState, useEffect } from 'react';
 import { Routes, Route } from "react-router-dom";
 import Cars from './pages/Cars';
 import Home from './pages/Home';
